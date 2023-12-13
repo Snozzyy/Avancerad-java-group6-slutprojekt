ae94e4f2a7ef1f46f62d00bae96e6c73fc78c4d9
Snozzyy <oliverschuller@hotmail.com>
2023-12-13 10:54:59 +0100

    Create repo

---
+++ src/main/java/com/example/avanceradjavagroup6slutprojekt/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.avanceradjavagroup6slutprojekt;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.avanceradjavagroup6slutprojekt {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.avanceradjavagroup6slutprojekt to javafx.fxml;
+    exports com.example.avanceradjavagroup6slutprojekt;
+}
